bc3187a557776ed8f4afdc650ae0fcd96d635ff9
Yuriy Artemyev <anuvyklack@gmail.com>
2021-12-02 00:08:08 +0300

    nvim: Return nvim-tree plugin

--- roles/neovim/files/lua/plugins_config/nvim-tree.lua
+++ roles/neovim/files/lua/plugins_config/nvim-tree.lua
@@ -1,16 +1,9 @@
 local g = vim.g
 
-g.nvim_tree_ignore = { '.git', 'tags', '.netrwhist' }
-
-g.nvim_tree_gitignore = 1  -- 0 by default
-
 g.nvim_tree_quit_on_open = 0 -- Closes the tree when you open a file.
 
 -- Display indent markers when folders are open.
 g.nvim_tree_indent_markers = 1
-
--- Hides files and folders starting with a dot.
-g.nvim_tree_hide_dotfiles = 1
 
 -- Highlight changed git files.
 g.nvim_tree_git_hl = 0
@@ -35,10 +28,10 @@ g.nvim_tree_special_files = {
 }
 
 -- Used as a separator between symlinks' source and target.
-g.nvim_tree_symlink_arrow = ' ➛ '
+g.nvim_tree_symlink_arrow = ' -> '
 
 -- Will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
-g.nvim_tree_respect_buf_cwd = 0
+g.nvim_tree_respect_buf_cwd = 1
 
 g.nvim_tree_show_icons = {
   git = 0,
@@ -94,14 +87,9 @@ g.nvim_tree_icons = {
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
 require('nvim-tree').setup {
-   -- Disables netrw completely.
-   disable_netrw = true,
-
-   -- Hijack netrw window on startup.
-   hijack_netrw = true,
-
-   -- Open the tree when running this setup function.
-   open_on_setup = false,
+   disable_netrw = true,  -- Disables netrw completely.
+   hijack_netrw = true,   -- Hijack netrw window on startup.
+   open_on_setup = false, -- Open the tree when running this setup function.
 
    -- Will not open on setup if the filetype is in this list.
    ignore_ft_on_setup = {},
@@ -114,28 +102,25 @@ require('nvim-tree').setup {
    open_on_tab = false,
 
    -- Hijack the cursor in the tree to put it at the start of the filename.
-   hijack_cursor = false,
+   hijack_cursor = true,
 
-   -- Updates the root directory of the tree on `DirChanged` (when your run
-   -- `:cd` usually).
-   update_cwd = false,
-
-   -- Show lsp diagnostics in the signcolumn.
-   diagnostics = {
+   diagnostics = {  -- Show lsp diagnostics in the signcolumn.
       enable = false,
       icons = { hint = "", info = "", warning = "", error = "" }
    },
 
+   update_cwd = true, -- Updates the root directory of the tree on `DirChanged`
+                      -- (when your run `:cd` usually).
+
    -- Update the focused file on `BufEnter`, un-collapses the folders
    -- recursively until it finds the file.
    update_focused_file = {
-      -- Enables the feature.
-      enable = false,
+      enable = true,  -- Enables the feature.
       -- Update the root directory of the tree to the one of the folder
       -- containing the file if the file is not under the current root
       -- directory.
       -- Only relevant when `update_focused_file.enable` is true.
-      update_cwd = false,
+      update_cwd = true,
       -- List of buffer names / filetypes that will not update the cwd if the
       -- file isn't found under the current root directory.
       -- Only relevant when `update_focused_file.update_cwd` is true and
@@ -150,6 +135,11 @@ require('nvim-tree').setup {
       cmd = nil,
       -- The command arguments as a list.
       args = {}
+   },
+
+   git = {  -- git integration with icons and colors
+      enable = true,
+      ignore = true, -- ignore files based on `.gitignore`
    },
 
    view = {
@@ -168,7 +158,12 @@ require('nvim-tree').setup {
             { key = '?', cb = tree_cb("toggle_help") }  -- help UI
          }
       }
-   }
+   },
+
+   filters = {
+      dotfiles = true,  -- Hides files and folders starting with a dot.
+      custom = { '.git', 'tags', '.netrwhist' }
+   },
 }
 
 require('keybindings').nvim_tree()
--- roles/neovim/files/plugin/plugins.lua
+++ roles/neovim/files/plugin/plugins.lua
@@ -730,10 +730,10 @@ return require('packer').startup(function()
    --                   File manager                   {{{
    -------------------------------------------------------
 
-    -- use { 'kyazdani42/nvim-tree.lua',
-    --    requires = 'kyazdani42/nvim-web-devicons',
-    --    config = function() require('plugins_config/nvim-tree') end
-    -- }
+   use { 'kyazdani42/nvim-tree.lua',
+      requires = 'kyazdani42/nvim-web-devicons',
+      config = function() require('plugins_config/nvim-tree') end
+   }
 
    -- use 'tpope/vim-vinegar'
 
