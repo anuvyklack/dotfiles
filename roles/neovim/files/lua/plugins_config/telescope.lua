ed7dbdc8427265cb661ddf3216d1e0be90c73daf
Yuriy Artemyev <anuvyklack@gmail.com>
2022-01-30 03:21:38 +0300

    vim: Update telescope settngs

--- roles/neovim/files/lua/plugins_config/telescope.lua
+++ roles/neovim/files/lua/plugins_config/telescope.lua
@@ -68,9 +68,7 @@ function custom_actions.multi_selection_open(prompt_bufnr)
     custom_actions._multiopen(prompt_bufnr, "edit")
 end
 
---------------------------------------------------------------------------------
---                             Telescope settngs                              --
---------------------------------------------------------------------------------
+--------------------------- Telescope settngs ----------------------------------
 
 telescope.setup {
    defaults = {
@@ -99,9 +97,27 @@ telescope.setup {
             -- ["<C-k>"] = require('telescope.actions').move_selection_better,
             ["<C-k>"] = 'move_selection_previous',
             ["<C-j>"] = 'move_selection_next',
+
+         -- ["<esc>"] = actions.close,
+         -- ["<C-j>"] = actions.move_selection_next,
+         -- ["<C-k>"] = actions.move_selection_previous,
+         -- ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+         -- ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+         -- ["<cr>"] = custom_actions.multi_selection_open,
+         -- ["<c-v>"] = custom_actions.multi_selection_open_vsplit,
+         -- ["<c-s>"] = custom_actions.multi_selection_open_split,
+         -- ["<c-t>"] = custom_actions.multi_selection_open_tab,
          },
          n = {  -- normal mode
             ["<Space>"] = 'toggle_selection',
+
+         -- ["<esc>"] = actions.close,
+         -- ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
+         -- ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
+         -- ["<cr>"] = custom_actions.multi_selection_open,
+         -- ["<c-v>"] = custom_actions.multi_selection_open_vsplit,
+         -- ["<c-s>"] = custom_actions.multi_selection_open_split,
+         -- ["<c-t>"] = custom_actions.multi_selection_open_tab,
          },
       },
    },
@@ -269,35 +285,13 @@ telescope.setup {
    }
 }
 
--- require('telescope').setup({
---    defaults = {
---       mappings = {
---          i = {
---             ["<esc>"] = actions.close,
---             ["<C-j>"] = actions.move_selection_next,
---             ["<C-k>"] = actions.move_selection_previous,
---             ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
---             ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
---             ["<cr>"] = custom_actions.multi_selection_open,
---             ["<c-v>"] = custom_actions.multi_selection_open_vsplit,
---             ["<c-s>"] = custom_actions.multi_selection_open_split,
---             ["<c-t>"] = custom_actions.multi_selection_open_tab,
---          },
---          n = {
---             ["<esc>"] = actions.close,
---             ["<tab>"] = actions.toggle_selection + actions.move_selection_next,
---             ["<s-tab>"] = actions.toggle_selection + actions.move_selection_previous,
---             ["<cr>"] = custom_actions.multi_selection_open,
---             ["<c-v>"] = custom_actions.multi_selection_open_vsplit,
---             ["<c-s>"] = custom_actions.multi_selection_open_split,
---             ["<c-t>"] = custom_actions.multi_selection_open_tab,
---          }
---       },
---    }
--- }
+------------------------------ Extensions --------------------------------------
 
 telescope.load_extension('fzf')  -- use fzf module in C
-telescope.load_extension('projects')
+
+local project_available, _ = pcall(require, 'project_nvim')
+if project_available then telescope.load_extension('projects') end
+
 -- telescope.load_extension('neoclip')
 
 require('keybindings').telescope()
