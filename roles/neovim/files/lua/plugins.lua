e3918ba99a17001b7992a7b5f5cfc62c0f4870f1
Yuriy Artemyev <anuvyklack@gmail.com>
2022-06-24 21:19:24 +0300

    feat(vim): New markdown plugins

--- roles/neovim/files/lua/plugins.lua
+++ roles/neovim/files/lua/plugins.lua
@@ -933,6 +933,34 @@ use { 'tweekmonster/startuptime.vim', as = 'startuptime',
    cmd = 'StartupTime'
 }
 
+use { 'frabjous/knap',
+   config = function()
+      vim.g.knap_settings = {
+         mdtohtml = "pandoc --standalone %docroot% -o %outputfile% --css ~/.config/nvim/assets/github-pandoc.css",
+      }
+
+      -- F5 processes the document once, and refreshes the view
+      vim.keymap.set('i','<F5>', require("knap").process_once)
+      vim.keymap.set('v','<F5>', require("knap").process_once)
+      vim.keymap.set('n','<F5>', require("knap").process_once)
+
+      -- F6 closes the viewer application, and allows settings to be reset
+      vim.keymap.set('i','<F6>', require("knap").close_viewer)
+      vim.keymap.set('v','<F6>', require("knap").close_viewer)
+      vim.keymap.set('n','<F6>', require("knap").close_viewer)
+
+      -- F7 toggles the auto-processing on and off
+      vim.keymap.set('i','<F7>', require("knap").toggle_autopreviewing)
+      vim.keymap.set('v','<F7>', require("knap").toggle_autopreviewing)
+      vim.keymap.set('n','<F7>', require("knap").toggle_autopreviewing)
+
+      -- F8 invokes a SyncTeX forward search, or similar, where appropriate
+      vim.keymap.set('i','<F8>', require("knap").forward_jump)
+      vim.keymap.set('v','<F8>', require("knap").forward_jump)
+      vim.keymap.set('n','<F8>', require("knap").forward_jump)
+   end
+}
+
 ----------------------------------------------------------------------}}}
 
 --                              Syntax                                {{{
@@ -979,6 +1007,8 @@ use { 'anuvyklack/vim-dealii-prm', as = 'syntax-dealii-prm',
 
 -- Kitty terminal conf file syntax highlight.
 use { "fladson/vim-kitty", as = 'syntax-kitty-conf' }
+
+use { 'mzlogin/vim-markdown-toc', ft = 'markdown' }
 
 ----------------------------------------------------------------------}}}
 
