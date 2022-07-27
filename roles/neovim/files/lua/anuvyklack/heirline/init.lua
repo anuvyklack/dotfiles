dc2cb0c94a70eeea9fda063ff4937abcdd3eefde
Yuriy Artemyev <anuvyklack@gmail.com>
2022-07-27 23:20:18 +0300

    vim: Update statusline

--- roles/neovim/files/lua/anuvyklack/heirline/init.lua
+++ roles/neovim/files/lua/anuvyklack/heirline/init.lua
@@ -532,12 +532,14 @@ local SearchResults = {
 }
 
 local Ruler = {
+   -- :help 'statusline'
+   -- ------------------
    -- %-2 : make item takes at least 2 cells and be left justified
    -- %l  : current line number
    -- %L  : number of lines in the buffer
    -- %c  : column number
-   -- provider = ' %7(%l:%3L%)  %-2c ',
-   provider = ' %7(%l:%L%)  %-2c ',
+   -- %V  : virtual column number as -{num}.  Not displayed if equal to '%c'.
+   provider = ' %7(%l:%L%)  %-5(%c%V%) ',
    hl = { bold = true }
 }
 
