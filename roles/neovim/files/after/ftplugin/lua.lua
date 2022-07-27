56ce5e05d30c63516f002279083c17b62fb96d22
Yuriy Artemyev <anuvyklack@gmail.com>
2022-07-27 23:21:33 +0300

    vim: Update virtual text for folds in Lua buffers

--- roles/neovim/files/after/ftplugin/lua.lua
+++ roles/neovim/files/after/ftplugin/lua.lua
@@ -30,53 +30,80 @@ vim.keymap.set({'n','x'}, 'gK', 'K', { buffer = true, desc = 'Show :help' })
 
 -- Ufo -------------------------------------------------------------------------
 
-local lua_ts_handler = function(virt_text, lnum, end_lnum, available_width, truncate, ctx)
+---@class ufo.TextChunk
+---@field [1] string Text
+---@field [2] string | integer Highlight name of ID
+
+---@class ufo.FoldContext
+---@field text string
+---@field end_virt_text ufo.TextChunk[]
+---@field winid integer
+---@field bufnr integer
+
+---@param virt_text ufo.TextChunk[]
+---@param lnum integer
+---@param end_lnum integer
+---@param available_width integer
+---@param truncate function
+---@param context ufo.FoldContext
+---@return ufo.TextChunk[]
+local lua_ts_handler = function(virt_text, lnum, end_lnum, available_width, truncate, context)
    -- local folded_sign = '⋯'
    local folded_sign = '…'
    -- local folded_sign = '...'
 
+   local termguicolors = vim.o.termguicolors --[[@as boolean]]
+
    -- print(vim.api.nvim_get_hl_id_by_name('luaTSComment'))
    -- P(virt_text)
+   -- P(context)
 
-   local original_text_with = ctx.text:len()
-   local comment_hl = vim.api.nvim_get_hl_by_name('Comment', vim.o.termguicolors)
+   ---@type integer
+   local original_text_with = context.text:len()
+
    local comment_tokens = { '--' }
-   local fold_marker = vim.opt.foldmarker:get()[1]
+   local comment_hl = vim.api.nvim_get_hl_by_name('Comment', termguicolors)
+   local fold_marker = vim.opt.foldmarker:get()[1] ---@type string
    local lua_patterns = {
-      empty = '^%s+$',
+      empty_str = '^%s+$',
+      spaces_at_beginning = '^%s+',
       fold_marker = table.concat{ '%s?', vim.pesc(fold_marker), '%d*' },
       only_comment_token = {}
    }
    for _, token in ipairs(comment_tokens) do
-      token = vim.pesc(token)
-      table.insert(
-         lua_patterns.only_comment_token,
-         table.concat{ '^%s*', token, '%s*$' }
-      )
+      table.insert(lua_patterns.only_comment_token,
+                   table.concat{ '^%s*', vim.pesc(token), '%s*$' })
    end
 
+   ---@type ufo.TextChunk[]
    local new_virt_text = {}
-   local cur_width = 0
+   local nvrt_width = 0 -- new_virt_text width
 
+   ---@param chunk ufo.TextChunk
+   ---@return boolean
    local function is_comment(chunk)
       local hl_id = chunk[2]
       if type(hl_id) == 'number' then
-         local hl = vim.api.nvim_get_hl_by_id(hl_id, vim.o.termguicolors)
+         local hl = vim.api.nvim_get_hl_by_id(hl_id, termguicolors)
          return vim.deep_equal(hl, comment_hl)
       else
          return false
       end
    end
 
+   ---@param chunk ufo.TextChunk
+   ---@return boolean
    local function is_empty(chunk)
       local text = chunk[1]
-      if text:find(lua_patterns.empty) then
+      if text:find(lua_patterns.empty_str) then
          return true
       else
          return false
       end
    end
 
+   ---@param chunk ufo.TextChunk
+   ---@return boolean continue Can we continue add chunks?
    local function add_chunk(chunk)
       -- Remove fold marker from comment
       if is_comment(chunk) then
@@ -91,11 +118,11 @@ local lua_ts_handler = function(virt_text, lnum, end_lnum, available_width, trun
       local continue = true
       local chunk_text = chunk[1]
       local chunk_width = vim.fn.strdisplaywidth(chunk_text)
-      if available_width > cur_width + chunk_width then
+      if available_width > nvrt_width + chunk_width then
          table.insert(new_virt_text, chunk)
          continue = true
       else
-         chunk_text = truncate(chunk_text, available_width - cur_width)
+         chunk_text = truncate(chunk_text, available_width - nvrt_width)
          chunk_width = vim.fn.strdisplaywidth(chunk_text)
          local hl_group = chunk[2]
          table.insert(new_virt_text, { chunk_text, hl_group })
@@ -107,19 +134,36 @@ local lua_ts_handler = function(virt_text, lnum, end_lnum, available_width, trun
          -- end
 
       end
-      cur_width = cur_width + chunk_width
+      nvrt_width = nvrt_width + chunk_width
       return continue
    end
 
    local function add_end_virt_text()
+      -- local first_chunk = context.end_virt_text[1]
+      -- first_chunk[1] = first_chunk[1]:gsub(lua_patterns.spaces_at_beginning, '')
+
+      local last_chunk = context.end_virt_text[#context.end_virt_text]
+      if last_chunk and is_comment(last_chunk) then
+         table.remove(context.end_virt_text)
+      end
+      last_chunk = context.end_virt_text[#context.end_virt_text]
+      if last_chunk and is_empty(last_chunk) then
+         table.remove(context.end_virt_text)
+      end
+
+      -- for i, chunk in ipairs(context.end_virt_text) do
+      --    if not (i == 1 and is_empty(chunk))
+      --       and not (i == #context.end_virt_text and is_comment(chunk))
+      --    then
+      --       continue = add_chunk(chunk)
+      --       if not continue then break end
+      --    end
+      -- end
+
       local continue = true
-      for i, chunk in ipairs(ctx.end_virt_text) do
-         if not (i == 1 and is_empty(chunk)) and
-            not (i == #ctx.end_virt_text and is_comment(chunk))
-         then
-            continue = add_chunk(chunk)
-            if not continue then break end
-         end
+      for _, chunk in ipairs(context.end_virt_text) do
+         continue = add_chunk(chunk)
+         if not continue then break end
       end
       return continue
    end
@@ -141,8 +185,8 @@ local lua_ts_handler = function(virt_text, lnum, end_lnum, available_width, trun
       if not continue then break end
    end
 
-   if cur_width < original_text_with then
-      add_chunk({string.rep(' ', original_text_with - cur_width), 'UfoFoldedFg'})
+   if nvrt_width < original_text_with then
+      add_chunk({string.rep(' ', original_text_with - nvrt_width), 'UfoFoldedFg'})
    end
 
    -- if continue then
@@ -155,6 +199,6 @@ local lua_ts_handler = function(virt_text, lnum, end_lnum, available_width, trun
    return new_virt_text
 end
 
-local prequire = require('util').prequire
 prequire('ufo').setFoldVirtTextHandler(nil, lua_ts_handler)
 
+-- vim: fml=4
