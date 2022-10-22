7f4ad0b8eae30eff14606029052be257292e0ee9
Yuriy Artemyev <anuvyklack@gmail.com>
2022-10-22 15:34:54 +0600

    refactor(vim): Update mini config

--- roles/neovim/files/assets/mini.lua
+++ roles/neovim/files/assets/mini.lua
@@ -1,11 +1,15 @@
--- Mini-config file for debugging.  To use run from shell:
--- nvim --clean -u mini.lua mini.lua
-
+---
+--- Mini-config file for debugging.  To use run from shell:
+--- nvim --clean -u mini.lua mini.lua
+---
 vim.o.packpath = '~/.local/share/nvim/site'
 
-vim.cmd 'packadd yanky'
-require('yanky').setup({
-  system_clipboard = {
-    sync_with_ring = false,
-  },
-})
+-- vim.cmd 'packadd yanky'
+-- require('yanky').setup({
+--   system_clipboard = {
+--     sync_with_ring = false,
+--   },
+-- })
+
+vim.cmd 'packadd telescope'
+require('telescope')
