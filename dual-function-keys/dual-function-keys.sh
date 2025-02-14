4af3e759a3c76f2211bd7ae0eb2c78ea961ae159
Yuriy Artemyev <anuvyklack@gmail.com>
2025-02-14 17:52:47 +0300

    update

--- dual-function-keys/dual-function-keys.sh
+++ dual-function-keys/dual-function-keys.sh
@@ -1,26 +1,93 @@
 #!/usr/bin/env bash
 
-export DOTFILES_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/chezmoi"
-source "$DOTFILES_DIR/functions/utils.sh"
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export DOTFILES="${DOTFILES:-XDG_DATA_HOME/chezmoi}"
+export SOFTWARE="${SOFTWARE_DIR:-$HOME/soft}"
+export STOW_DIR="${STOW_DIR:-/usr/local/stow}"
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$script_dir/interception-tools.sh"
+[ ! -d "$STOW_DIR" ] && sudo mkdir -p "$STOW_DIR"
+
+log() {
+    echo -n "$(color $1 bold $2) "
+    shift 2
+    echo "$*"
+}
+
+which_os() {
+    if [[ "$(uname -s)" == "Darwin" ]]; then
+        echo "darwin"
+    elif [[ -f /etc/os-release ]]; then
+        source /etc/os-release
+        echo "$ID"
+    else
+        echo "unknown"
+    fi
+}
+
+
+interception_tools() {
+    # set -x
+    local app="interception-tools"
+    local repo_url="https://gitlab.com/interception/linux/tools.git"
+
+    local src_dir="$SOFTWARE/$app"
+    local build_dir="$src_dir/build"
+    local install_dir="$STOW_DIR/$app"
+
+    log blue "Install prerequisites"
+    case $(which_os) in
+        fedora)
+            sudo dnf install -y cmake libevdev-devel systemd-devel yaml-cpp-devel boost-devel stow
+            ;;
+        debian|ubuntu)
+            sudo apt-get install -y cmake libevdev-dev libudev-dev libyaml-cpp-dev libboost-dev stow
+            ;;
+        *)
+            log red "Unsapported OS: $(which_os)"
+            exit 1
+            ;;
+    esac
+
+    if git clone-or-pull "$repo_url" "$src_dir"; then
+        [ -d $build_dir ] && rm -rf $build_dir
+        mkdir $build_dir
+        pushd $build_dir
+        log blue "Building" $app
+        cmake -DCMAKE_BUILD_TYPE=Release \
+              -DCMAKE_INSTALL_PREFIX=$install_dir \
+              -S $src_dir -B $build_dir
+        make
+        log blue "Installing" $app
+        sudo make install
+        log blue "Stow" $app
+        sudo stow --dir=$STOW_DIR -R $app
+        log blue "Remove $build_dir"
+        rm -rf $build_dir
+        popd
+    fi
+
+    [ ! -d "/etc/$app" ] && sudo mkdir -p "/etc/$app"
+    # set +x
+    return 0
+}
+
 
 dual_function_keys() {
     # set -x
     local app="dual-function-keys"
     local repo_url="https://gitlab.com/interception/linux/plugins/dual-function-keys.git"
 
-    local src_dir="$SOFTWARE_DIR/$app"
+    local src_dir="$SOFTWARE/$app"
     local install_dir="$STOW_DIR/$app"
 
-    if git_clone_or_pull "$repo_url" "$src_dir"; then
+    if git clone-or-pull "$repo_url" "$src_dir"; then
         pushd $src_dir > /dev/null
-        log_blue "Building" $app
+        log blue "Building" $app
         make
-        log_blue "Installing" $app
+        log blue "Installing" $app
         sudo make install PREFIX=$install_dir
-        log_blue "Stow" $app
+        log blue "Stow" $app
         sudo stow --dir=$STOW_DIR -R $app
         make clean
         popd > /dev/null
@@ -37,18 +104,19 @@ copy_config_files() {
         "kinesis-advantage2"
         "laptop-keyboard"
     )
-    log_blue "Copy config files"
+    log blue "Copy config files"
     for file in "${files[@]}"; do
         file="$file.yaml"
         sudo cp "$config_files/$file" "/etc/interception-tools/$file"
     done
-    log_green "Done"
+    log green "Done"
     sudo cp "$config_files/udevmon.yaml" "/etc/udevmon.yaml"
     sudo cp "$config_files/udevmon.service" "/etc/systemd/system/udevmon.service"
-    log_blue "Enable udevmon systemd service"
+    log blue "Enable udevmon systemd service"
     sudo systemctl enable --now udevmon
-    log_green "Done"
+    log green "Done"
 }
 
+interception_tools
 dual_function_keys
 copy_config_files
