862d18e6db251fce4711e3e484f8b71d880683d2
Yuriy Artemyev <anuvyklack@gmail.com>
2025-02-22 18:24:47 +0300

    feat(emacs): Add Lua support

--- home/dot_config/doom/init.el
+++ home/dot_config/doom/init.el
@@ -152,7 +152,7 @@
        (latex +cdlatex +fold +lsp) ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       (lua +fennel +lsp +tree-sitter) ; one-based indices? one-based indices
        markdown            ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        nix                 ; I hereby declare "nix geht mehr!"
