b5b4f72ef29fd49be1bdfdf43ee7a96b7b22c5df
Yuriy Artemyev <anuvyklack@gmail.com>
2025-02-26 22:03:36 +0300

    refactor(fish): move conda related code to separate file

---
+++ home/dot_config/fish/conf.d/miniconda3.fish
@@ -0,0 +1,13 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
--- home/dot_config/fish/private_config.fish
+++ home/dot_config/fish/private_config.fish
@@ -103,16 +103,3 @@ if status is-interactive
 
 end # status is-interactive
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/miniconda3/bin/conda
-    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/opt/miniconda3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
